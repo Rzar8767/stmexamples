ffe56eb3214ae6fc02d75ffed3d76dc5d0eebbd1
Marcin Wiktorowski <wiktorowski211@gmail.com>
2018-01-18 22:05:52 +0100

    Timer in ms

--- biblioteka/timer.c
+++ biblioteka/timer.c
@@ -5,21 +5,14 @@
 void initTimerAdv(uint16_t presc, uint32_t period, TIM_TypeDef* TIMx, FunctionalState state)
 {
 	uint32_t RCC_APB1Periph_TIMx;
-	switch(TIMx)
-	{
-	  case TIM2:
+	if(TIMx == TIM2)
 		  	  RCC_APB1Periph_TIMx = RCC_APB1Periph_TIM2;
-		  break;
-	  case TIM3:
+	else if(TIMx ==  TIM3)
 	  		  RCC_APB1Periph_TIMx = RCC_APB1Periph_TIM3;
-	  	  break;
-	  case TIM4:
+	else if(TIMx ==  TIM4)
 	  		  RCC_APB1Periph_TIMx = RCC_APB1Periph_TIM4;
-	  	  break;
-	  case TIM5:
+	else if(TIMx ==  TIM5)
 	  		  RCC_APB1Periph_TIMx = RCC_APB1Periph_TIM5;
-	  	  break;
-	}
 
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIMx, ENABLE );
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure; //84mhz 84000000
@@ -31,10 +24,10 @@ void initTimerAdv(uint16_t presc, uint32_t period, TIM_TypeDef* TIMx, Functional
 }
 
 // example: initTime(250, TIM4, ENABLE); wlaczy timer na 0.25s
-void initTime(uint8_t miliseconds, TIM_TypeDef* TIMx, FunctionalState state)
+void initTime(uint16_t miliseconds, TIM_TypeDef* TIMx, FunctionalState state)
 {
 	uint32_t period;
 	period = (miliseconds*10)-1;
 
-	initTimerAdv(8399, period, TIMx, ENABLE)
+	initTimerAdv(8399, period, TIMx, ENABLE);
 }
