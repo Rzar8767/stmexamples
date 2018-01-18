c0f0f4bc5e6e87633572fd98b482de855c4051fd
Marcin Wiktorowski <wiktorowski211@gmail.com>
2018-01-18 21:14:24 +0100

    Function names

--- biblioteka/onboard.c
+++ biblioteka/onboard.c
@@ -4,18 +4,18 @@
 
 
 void initOnBoardDiodes();
-void GPIO_Config(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins);
-void GPIO_ConfigIO(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins, GPIOMode_TypeDef Mode, GPIOPuPd_TypeDef PuPd);
+void initGPIO(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins);
+void initGPIOext(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins, GPIOMode_TypeDef Mode, GPIOPuPd_TypeDef PuPd);
 
 // PD12 = GREEN; PD13 = ORANGE; PD14 = RED; PD15 = BLUE;
 // piny PD12, PD13, PD14, PD15
 void initOnBoardDiodes() {
-	GPIO_Config(RCC_AHB1Periph_GPIOD,GPIOD,GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+	initGPIO(RCC_AHB1Periph_GPIOD,GPIOD,GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
 }
 
-// Enables GPIO in input/output mode
+// Initializes GPIO in input/output mode
 // GPIO_Config(RCC_AHB1Periph_GPIOD,GPIOD,GPIO_Pin_12 | GPIO_Pin_13,GPIO_Mode_OUT,GPIO_PuPd_NOPULL);
-void GPIO_ConfigIO(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins, GPIOMode_TypeDef Mode, GPIOPuPd_TypeDef PuPd){
+void initGPIOext(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins, GPIOMode_TypeDef Mode, GPIOPuPd_TypeDef PuPd){
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOx, ENABLE);
 
 	GPIO_InitTypeDef  GPIO_InitStructure;
@@ -27,9 +27,9 @@ void GPIO_ConfigIO(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t 
 	GPIO_Init(GPIOx, &GPIO_InitStructure);
 }
 
-// Enables GPIO in output mode
-// GPIO_Config(RCC_AHB1Periph_GPIOD,GPIOD,GPIO_Pin_12 | GPIO_Pin_13);
-void GPIO_Config(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins){
-			GPIO_ConfigIO(RCC_AHB1Periph_GPIOx,GPIOx,Pins, GPIO_Mode_OUT,GPIO_PuPd_NOPULL);
+// Initializes GPIO in output mode
+// initGPIO(RCC_AHB1Periph_GPIOD,GPIOD,GPIO_Pin_12 | GPIO_Pin_13);
+void initGPIO(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins){
+	initGPIOext(RCC_AHB1Periph_GPIOx,GPIOx,Pins, GPIO_Mode_OUT,GPIO_PuPd_NOPULL);
 }
 
