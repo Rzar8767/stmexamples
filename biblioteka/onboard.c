89755487d776e20d4add1b4ccb0081ad3943eeb4
Marcin Wiktorowski <wiktorowski211@gmail.com>
2018-01-19 09:22:44 +0100

    Exti IRQ

--- biblioteka/onboard.c
+++ biblioteka/onboard.c
@@ -4,8 +4,11 @@
 
 
 void initOnBoardDiodes();
+void initOnBoardButton();
+void initOnBoardButtonIRQ();
 void initGPIO(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins);
 void initGPIOadv(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins, GPIOMode_TypeDef Mode, GPIOPuPd_TypeDef PuPd);
+void initExtiIRQ(uint8_t EXTx_IRQn, uint32_t EXTI_Linex,uint8_t EXTI_PortSourceGPIOx, uint8_t EXTI_PinSourcex);
 
 // PD12 = GREEN; PD13 = ORANGE; PD14 = RED; PD15 = BLUE;
 // pins PD12, PD13, PD14, PD15
@@ -39,14 +42,14 @@ void initGPIOadv(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pi
 void initGPIO(uint32_t RCC_AHB1Periph_GPIOx,GPIO_TypeDef* GPIOx , uint32_t Pins){
 	initGPIOadv(RCC_AHB1Periph_GPIOx,GPIOx,Pins, GPIO_Mode_OUT,GPIO_PuPd_NOPULL);
 }
-
-void initExtiIRQ(){
+//   initExtiIRQ(EXTI0_IRQn, EXTI_Line0,EXTI_PortSourceGPIOA, EXTI_PinSource0); uruchomienie przerwania User Buttona
+void initExtiIRQ(uint8_t EXTx_IRQn, uint32_t EXTI_Linex,uint8_t EXTI_PortSourceGPIOx, uint8_t EXTI_PinSourcex){
 
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 
 	NVIC_InitTypeDef NVIC_InitStructure;
 	// numer przerwania
-	NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannel = EXTx_IRQn;
 	// priorytet g³ówny
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
 	// subpriorytet
@@ -58,7 +61,7 @@ void initExtiIRQ(){
 
 	EXTI_InitTypeDef EXTI_InitStructure;
 	// wybór numeru aktualnie konfigurowanej linii przerwañ
-	EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+	EXTI_InitStructure.EXTI_Line = EXTI_Linex;
 	// wybór trybu - przerwanie b¹dŸ zdarzenie
 	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
 	// wybór zbocza, na które zareaguje przerwanie
@@ -69,5 +72,18 @@ void initExtiIRQ(){
 	EXTI_Init(&EXTI_InitStructure);
 
 	// pod³¹czenie danego pinu portu do kontrolera przerwañ
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOx, EXTI_PinSourcex);
 }
+
+void initOnBoardButtonIRQ(){
+	initExtiIRQ(EXTI0_IRQn, EXTI_Line0,EXTI_PortSourceGPIOA, EXTI_PinSource0);
+}
+/* Onboard button IRQ example
+void EXTI0_IRQHandler(void)
+{
+    if(EXTI_GetITStatus(EXTI_Line0) != RESET)
+    {
+    	//code
+        EXTI_ClearITPendingBit(EXTI_Line0);
+    }
+}*/
