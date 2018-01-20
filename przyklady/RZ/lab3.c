3bf05cf375eb4ff5c1237110ce6dab49c8d8f1cf
Marcin Wiktorowski <wiktorowski211@gmail.com>
2018-01-20 18:09:43 +0100

    Revert "Merge branch 'master' of https://github.com/Rzar8767/stmexamples"
    This reverts commit 87f20f3615154ce71a332c6d33cb7039b58ca521, reversing
    changes made to 87db323b1da04fecfb8d2cc0afc91cb2730b607e.

--- przyklady/RZ/lab3.c
+++ przyklady/RZ/lab3.c
@@ -1,145 +1,52 @@
+
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
 
-// requires timer.c
-// requires onboard.c
-// requires externDisplay.c
-// requires externButtons.c
-
 void initOnBoardDiodes();
-void initOnBoardButton();
+void initExternalDisplay();
 
-void initExternDisplay();
-void setDigit(int num);
-void displayNumber(int num);
-void initNumbers();
-
-//PWM
-void initPWM(uint32_t period, TIM_TypeDef* TIMx);
-void initPWMTime(uint32_t miliseconds, TIM_TypeDef* TIMx);
-void initPWMChannel(TIM_TypeDef* TIMx, uint8_t channel, GPIO_TypeDef* GPIOx,uint8_t GPIO_PinSourcex,uint32_t GPIO_Pin_x);
-void setPWMWidthPeriod(TIM_TypeDef* TIMx, uint8_t channel, uint8_t percent, uint16_t period);
-void setPWMWidthMiliseconds(TIM_TypeDef* TIMx, uint8_t channel, uint8_t percent, uint16_t miliseconds);
-void startPWM(TIM_TypeDef* TIMx);
-
-void initTimerTime(uint16_t miliseconds, TIM_TypeDef* TIMx, FunctionalState state);
-
+void enableTimer(uint16_t presc, uint32_t period, uint32_t RCC_APB1Periph_TIMx, TIM_TypeDef* TIMx, FunctionalState state );
 void zad2_part1();
 void zad3_part1();
-void zad4_part1();
-void zad5_part1();
 
-void zad1_part2();
-void zad2_part2();
-void zad3_part2();
-void zad4_part2();
 
-void numbersFrom0To9();
-
-// te takie plywajace po LED w kolo
-void swimmingSomething();
 
 int main(void)
 {
 	//zad2_part1();
-	//zad3_part1();
-
-	//swimmingSomething();
-	//numbersFrom0To9();
-	//zad2_part1();
-	//zad3_part1();
-	//zad4_part1();
-	//zad5_part1();
-
-	//zad1_part2();
-	//zad2_part2();
-	//zad3_part2();
-	zad4_part2();
-	while(1)
-	{
-
-	}
-
+	zad3_part1();
+	for(;;);
 }
 
-void numbersFrom0To9()
+// konfiguracja wybranego timera i mruganie kropka
+void zad3_part1()
 {
-	initOnBoardButton();
-
-	initExternDisplay();
-
-	// set which part of LED screen you want to see the number on
-	setDigit(1);
-	setDigit(2);
-	setDigit(3);
-	setDigit(4);
-
-	int buttonstate_o = 0;
-	int buttonstate=0;
-
-	int activeNum = 0;
-	displayNumber(activeNum);
-	++activeNum;
+	initExternalDisplay();
+	enableTimer(8399, 9999, RCC_APB1Periph_TIM3, TIM3, ENABLE);
+	uint32_t counter;
+	GPIO_SetBits(GPIOA,  GPIO_Pin_8);
 	while(1)
 	{
-		buttonstate = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
-		// when button pressed, contact oscillation not eliminated
-		if(buttonstate == 1 && buttonstate_o == 0)
-		{
-			displayNumber(activeNum);
-			++activeNum;
-			if(activeNum > 9) activeNum =0;
+		counter = TIM3->CNT;
+		if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update)) {
+			GPIO_ToggleBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7);
+			TIM_ClearFlag(TIM3, TIM_FLAG_Update);
 		}
-		buttonstate_o = buttonstate;
 	}
 }
 
-void swimmingSomething()
-{
-	initOnBoardButton();
-
-	initExternDisplay();
-
-	// set which part of LED screen you want to see the number on
-	setDigit(1);
-
-
-	int buttonstate_o = 0;
-	int buttonstate=0;
-
-	uint16_t activeSeg = 1;
-	GPIO_ResetBits(GPIOE, activeSeg);
-	while(1)
-	{
-		buttonstate = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
-		// when button pressed, contact oscillation not eliminated
-		if(buttonstate == 1 && buttonstate_o == 0)
-		{
-			GPIO_SetBits(GPIOE, activeSeg);
-			activeSeg = activeSeg << 1;
-			if(activeSeg > 32) activeSeg =1;
-			GPIO_ResetBits(GPIOE, activeSeg);
-
-		}
-		buttonstate_o = buttonstate;
-	}
-}
-
-
-
-
-
-
-
-
-// konfiguracja wybranego timera i mrugac diodami co sekunde
+// konfiguracja wybranego timera i mrugac diodami
 void zad2_part1()
 {
 	initOnBoardDiodes();
-	initTimerTime(1000, TIM3, ENABLE);
+	enableTimer(8399, 9999, RCC_APB1Periph_TIM3, TIM3, ENABLE);
+	uint32_t counter;
 	while(1)
 	{
-		//TIM3-> CCR1;
+		counter = TIM3->CNT;
+	//	if(counter > 3000)
+		//	GPIO_Set(GPIOD, GPIO_Pin_12| GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+
 		if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update)) {
 			GPIO_ToggleBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
 			TIM_ClearFlag(TIM3, TIM_FLAG_Update);
@@ -148,247 +55,57 @@ void zad2_part1()
 	}
 }
 
-// konfiguracja wybranego timera i mruganie kropka co sekunde
-void zad3_part1()
+// wyzwlanie co sekunde timera 4: (uzywac na timerach 2-5 i 12-14)
+//enableTimer(8399, 9999, RCC_APB1Periph_TIM4, TIM4, ENABLE);
+void enableTimer(uint16_t presc, uint32_t period, uint32_t RCC_APB1Periph_TIMx, TIM_TypeDef* TIMx, FunctionalState state )
 {
-	initExternDisplay();
-	initTimerTime(1000, TIM3, ENABLE);
-	setDigit(2);
-	while(1)
-	{
-		if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update)) {
-			GPIO_ToggleBits(GPIOE,  GPIO_Pin_7);
-			TIM_ClearFlag(TIM3, TIM_FLAG_Update);
-		}
-	}
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIMx, ENABLE );
+		TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure; //84mhz 84000000
+		TIM_TimeBaseStructure.TIM_Prescaler = presc;
+		TIM_TimeBaseStructure.TIM_Period = period;
+		TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+		TIM_TimeBaseStructure.TIM_CounterMode =  TIM_CounterMode_Up;
+		TIM_TimeBaseInit(TIMx, &TIM_TimeBaseStructure);
+		TIM_Cmd(TIMx, state);
 }
 
-// symulacja PWM na diodach bez PWM
-void zad4_part1()
+// piny PD12, PD13, PD14, PD15
+void initOnBoardDiodes()
 {
-	initOnBoardDiodes();
-	initTimerTime(1000, TIM3, ENABLE);
+	 /* GPIOD Periph clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	uint8_t D1passed =0;
-	uint8_t D2passed =0;
-	uint8_t D3passed =0;
-
-	while(1)
-	{
-		// 0.5 swieci
-		if(TIM3->CNT > 4999 && !D1passed)
-		{
-			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
-			D1passed = 1;
-		}
-		// 0.2 swieci
-		if(TIM3->CNT > 1999 && !D2passed)
-		{
-			GPIO_ResetBits(GPIOD, GPIO_Pin_13);
-			D2passed = 1;
-		}
-		// 0.7 swieci
-		if(TIM3->CNT > 6999 && !D3passed)
-		{
-			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
-			D3passed = 1;
-		}
-		if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update)) {
-			GPIO_SetBits(GPIOD,  GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14);
-			D1passed = 0;
-			D2passed = 0;
-			D3passed = 0;
-			TIM_ClearFlag(TIM3, TIM_FLAG_Update);
-		}
-	}
+    GPIO_InitTypeDef  GPIO_InitStructure;
+    /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 
-// wyswietlanie kolejnych cyfr co 0.5s
-void zad5_part1()
+/*
+// piny PA8 do zasilania wyswietlacza
+// piny PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7 for segments
+// P0 = a itd.
+     a
+   f   b
+	 g
+   e   c
+	 d   h
+ */
+void initExternalDisplay()
 {
+	 /* GPIOD Periph clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	GPIO_InitTypeDef  GPIO_InitStructure;
+   /* Configure PA... in output pushpull mode */
+   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2| GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8;
+   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+   GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	initExternDisplay();
-
-	initTimerTime(500,TIM3,ENABLE);
-
-	// set which part of LED screen you want to see the number on
-	setDigit(1);
-	setDigit(2);
-	setDigit(3);
-	setDigit(4);
-
-	int activeNum = 0;
-	displayNumber(activeNum);
-	++activeNum;
-	while(1)
-	{
-
-		// change number
-		if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update))
-		{
-			displayNumber(activeNum);
-			++activeNum;
-			if(activeNum > 9) activeNum =0;
-			TIM_ClearFlag(TIM3, TIM_FLAG_Update);
-		}
-
-	}
 }
-
-// swiecenie diodami za pomoca PWM, pierwsza 0.5s druga 0.2s, trzecia 0.7  na sekunde
-void zad1_part2()
-{
-	initOnBoardDiodes();
-
-	initPWMTime(1000, TIM4);
-
-	// TIM4 CH1 to dioda LED 12
-	initPWMChannel(TIM4, 1, GPIOD, GPIO_PinSource12, GPIO_Pin_12);
-	setPWMWidthMiliseconds(TIM4, 1, 50 , 1000);
-
-	// TIM4 CH2 to dioda LED 13
-	initPWMChannel(TIM4, 2, GPIOD, GPIO_PinSource13, GPIO_Pin_13);
-	setPWMWidthMiliseconds(TIM4, 2, 20 , 1000);
-
-	// TIM4 CH3 to dioda LED 14
-	initPWMChannel(TIM4, 3, GPIOD, GPIO_PinSource14, GPIO_Pin_14);
-	setPWMWidthMiliseconds(TIM4, 3, 70 , 1000);
-
-	startPWM(TIM4);
-
-	while(1)
-	{}
-}
-
-
-// swiecenie dioda za pomoca PWM, steruje jej jasnoscia, 1kHz
-void zad2_part2()
-{
-	initOnBoardDiodes();
-
-	//int hz = 1000;
-	//int freq = 1000/hz;
-
-	//initTimer
-	initPWMTime(1, TIM4);
-	initTimerTime(100, TIM3, ENABLE);
-
-
-	initPWMChannel(TIM4, 1, GPIOD, GPIO_PinSource12, GPIO_Pin_12);
-	//setPWMWidthMiliseconds(TIM4, 1, 44 , freq-1);
-
-	TIM4->CCR1 = 0;
-	startPWM(TIM4);
-
-	int dir=1;
-	int setCCR = 1;
-	while(1)
-	{
-     	if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update) != RESET)
-     	{
-        	TIM4->CCR1 = setCCR;
-        	// 9 is max brightness, 0 is off
-        	if(setCCR > 8 || setCCR < 1)
-        	{
-        		dir *= -1;
-
-        	}
-        	setCCR += dir;
-        	TIM_ClearFlag(TIM3, TIM_FLAG_Update);
-     	}
-	}
-}
-
-// swiecenie dioda RGB za pomoca PWM, steruje jej jasnoscia, 1kHz
-void zad3_part2()
-{
-	initGPIO(RCC_AHB1Periph_GPIOC, GPIOC, GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8);
-
-
-	//int hz = 1000;
-	//int freq = 1000/hz;
-
-	// 1kHz, can configure PWM from 1 to 100
-	initTimerPresc(839, 99, TIM3, DISABLE);
-
-	// RGB diode R
-	initPWMChannel(TIM3, 1, GPIOC, GPIO_PinSource6, GPIO_Pin_6);
-
-	// RGB diode G
-	initPWMChannel(TIM3, 2, GPIOC, GPIO_PinSource7, GPIO_Pin_7);
-
-	// RGB diode B
-	initPWMChannel(TIM3, 3, GPIOC, GPIO_PinSource8, GPIO_Pin_8);
-
-
-	TIM3->CCR1 = 10;
-	TIM3->CCR2 = 50;
-	TIM3->CCR3 = 75;
-	startPWM(TIM3);
-
-	int bDir =-1;
-
-	while(1)
-	{
-	}
-}
-
-// swiecenie dioda RGB za pomoca PWM, steruje jej jasnoscia, 1kHz, zmiana jasnosci co 10ms
-void zad4_part2()
-{
-	initGPIO(RCC_AHB1Periph_GPIOC, GPIOC, GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8);
-
-
-	//int hz = 1000;
-	//int freq = 1000/hz;
-
-	// 1kHz, can configure PWM from 1 to 100
-	initTimerPresc(839, 99, TIM3, DISABLE);
-
-	// RGB diode R
-	initPWMChannel(TIM3, 1, GPIOC, GPIO_PinSource6, GPIO_Pin_6);
-
-	// RGB diode G
-	initPWMChannel(TIM3, 2, GPIOC, GPIO_PinSource7, GPIO_Pin_7);
-
-	// RGB diode B
-	initPWMChannel(TIM3, 3, GPIOC, GPIO_PinSource8, GPIO_Pin_8);
-
-
-	TIM3->CCR1 = 10;
-	TIM3->CCR2 = 50;
-	TIM3->CCR3 = 75;
-	startPWM(TIM3);
-	int current = 0;
-	const int change = 10;
-	int rDir =1;
-	int gDir =1;
-	int bDir =-1;
-
-	while(1)
-	{
-     	if(TIM_GetFlagStatus(TIM3, TIM_FLAG_Update) != RESET)
-     	{
-			if(current > change)
-			{
-
-				if(TIM3->CCR1 > 99 || TIM3->CCR1 < 1)
-					rDir *= -1;
-				if(TIM3->CCR2 > 99 || TIM3->CCR2 < 1)
-					gDir *= -1;
-				if(TIM3->CCR3 > 99 || TIM3->CCR3 < 1)
-					bDir *= -1;
-
-				TIM3->CCR1 += rDir;
-				TIM3->CCR2 += gDir;
-				TIM3->CCR3 += bDir;
-
-				current = 0;
-			}
-			++current;
-			TIM_ClearFlag(TIM3, TIM_FLAG_Update);
-     	}
-	}
-}
-
-
